f8efc6e97e98d6c223a894963931723f140fd533
abbe2022 <103110080+abbe2022@users.noreply.github.com>
2026-05-29 13:31:26 +0200

    List

--- .venv/main.py
+++ .venv/main.py
@@ -1,12 +1,41 @@
-print('hej abbe')
-print('den här appen skapas av mig abbe')
+#print('hej abbe')
+#print('den här appen skapas av mig abbe')
 
 
-balance=int(input('skriv in siffran'))
-if  0 < balance >= 1:
-    print('det finns pengar')
-else:
-    print('det finns inga pengar')
-print('hej jag är defult hej')
+#balance=int(input('skriv in siffran'))
+#if  0 < balance >= 1:
+#    print('det finns pengar')
+#else:
+#    print('det finns inga pengar')
+#print('hej jag är defult hej')
 
 #hej hje hej
+
+
+#r=3
+
+#while r > 0:
+#    print(r)
+#    r -= 1
+#    print('inuti loopen')
+#print('efter loopen')
+
+#for i in range(5):
+#    print(f"i är {i}")
+#for j in range(10,20,2):
+#    print(f"j är {j}")
+
+user_name = ("Frodo")
+friends = ["Sam","marry","Pippin"]
+friends.append("Sameagol")
+print(friends)
+print(user_name)
+print(friends)
+
+print(f"frodo har {len(friends)} friends")
+
+print(f"{friends[0]} tycker inte om {friends[3]}")
+friends[-1]="Gollum"
+print(f"{friends[0]} tycker inte om {friends[3]}")
+friends.remove("Gollum")
+print(friends)
